3e9997199f672363bb24155bb11b4cf6ae7eb7d0
Jacques <soromoudorojacques38@gmail.com>
2021-04-08 17:58:44 -0300

    changement

--- lib/home.dart
+++ lib/home.dart
@@ -14,7 +14,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Soromou'),
+        title: Text('Jacques'),
       ),
       /* body: StreamBuilder<DocumentSnapshot>(
         stream: Firestore.instance
